81f72b2f1dbf02d42af2f202e013fa0b1365d240
PabloFriasCampos <p4blule@gmail.com>
2024-04-16 21:00:53 +0200

    Totales y aumentar/decrementar cantidad en carrito (no aplica en bbdd)

--- FrontClientes/src/app/carrito/carrito.component.ts
+++ FrontClientes/src/app/carrito/carrito.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { APIService } from '../services/api.service';
 import { Mesa } from '../models/mesa';
 import { ActivatedRoute } from '@angular/router';
+import { ListaProductos } from '../models/lista-productos';
 
 @Component({
   selector: 'app-carrito',
@@ -10,6 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CarritoComponent {
 
+  total: number = 0;
   mesa: Mesa = new Mesa();
 
   constructor(private api: APIService, private activatedRoute: ActivatedRoute) { }
@@ -18,6 +20,22 @@ export class CarritoComponent {
     const id = await this.activatedRoute.snapshot.paramMap.get('id');
     if (id) {
       this.mesa = await this.api.getMesa(+id);
+      this.calcularTotal()
+    }
+  }
+
+  changeCantidad(dif: number, producto: ListaProductos) {
+    producto.Cantidad += dif;
+    if (producto.Cantidad <= 0) {
+      this.mesa.Productos.splice(this.mesa.Productos.indexOf(producto), 1)
+    }
+    this.calcularTotal()
+  }
+
+  calcularTotal() {
+    this.total = 0;
+    for (let producto of this.mesa.Productos) {
+      this.total += +(producto.Cantidad * producto.Producto.Precio).toFixed(2);
     }
   }
 
